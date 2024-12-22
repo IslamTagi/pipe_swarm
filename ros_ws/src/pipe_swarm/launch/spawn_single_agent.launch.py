bb7bff4cc2de04733423132f297b5fddf7f3b45a
islamtagi <islamtagi@hotmail.com>
2024-12-22 22:32:58 +0300

    adding ros2 control to female linkage

--- ros_ws/src/pipe_swarm/launch/spawn_single_agent.launch.py
+++ ros_ws/src/pipe_swarm/launch/spawn_single_agent.launch.py
@@ -1,6 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from ament_index_python.packages import get_package_share_directory
@@ -11,41 +13,70 @@ def generate_launch_description():
     pipe_swarm_share = get_package_share_directory('pipe_swarm')
     gazebo_ros_share = get_package_share_directory('gazebo_ros')
     xacro_path = os.path.join(pipe_swarm_share, 'urdf', 'pipe_robot.urdf.xacro')
-    
+
     namespace = f'agent_n'
-    
-    return LaunchDescription([
-        # Include Gazebo launch file
-        IncludeLaunchDescription(
+
+    # Include Gazebo launch file
+    gazebo_include = IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(gazebo_ros_share, 'launch', 'gazebo.launch.py')
             )
-        ),
-
-        # Load and publish the robot state
-        Node
-        (
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            namespace=namespace,
-            parameters=[{'robot_description': Command(['xacro ', xacro_path, ' agent_namespace:=', 'agent_n'])}]
-        ),
-
-        # Spawn the robot entity in Gazebo
-        Node(
-            package='gazebo_ros',
-            executable='spawn_entity.py',
-            name='spawn_agent',
-            namespace=namespace,
-            output='screen',
-            arguments=['-topic', 'robot_description', '-entity', 'pipe_robot']
-        ),
-
-        # Launch RViz2 for visualization
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            output='screen'
         )
+    
+    # Load and publish the robot state
+    pipe_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        # namespace=namespace,
+        parameters=[{'robot_description': Command(['xacro ', xacro_path, ' agent_namespace:=', namespace])}]
+    )
+
+    # Spawn the robot entity in Gazebo
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        # namespace=namespace,
+        arguments=['-topic', 'robot_description',
+                    '-entity', 'pipe_robot'],
+        output='screen'
+    )
+
+    # Launch RViz2 for visualization
+    rviz_display = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen'
+    )
+
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'position_controller'],
+        output='screen'
+    )
+    
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_joint_trajectory_controller],
+            )
+        ),
+        gazebo_include,
+        pipe_robot_state_publisher,
+        spawn_entity,
+        rviz_display
     ])
