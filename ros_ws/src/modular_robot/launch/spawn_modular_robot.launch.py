c69f92b627d7040163c2eecad3ee679bec291d35
islamtagi <islamtagi@hotmail.com>
2025-03-11 00:09:09 +0000

    updating female linkage with stl

--- ros_ws/src/modular_robot/launch/spawn_modular_robot.launch.py
+++ ros_ws/src/modular_robot/launch/spawn_modular_robot.launch.py
@@ -72,8 +72,8 @@ def generate_launch_description():
     )
 
     joint_state_publisher = Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui'
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui'
     )
 
     agents.extend([
