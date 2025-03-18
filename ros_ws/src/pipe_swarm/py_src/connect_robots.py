91caf995aaec15d0f257f4a284d8f5a0e7b7c696
Fintan <mn20fl@leeds.ac.uk>
2025-03-18 13:53:42 +0000

    Updated connect robots

--- ros_ws/src/pipe_swarm/py_src/connect_robots.py
+++ ros_ws/src/pipe_swarm/py_src/connect_robots.py
@@ -6,56 +6,95 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
 import numpy as np
+from sensor_msgs.msg import Imu
+import math
 
-class MyNode(Node):
+class Connect_Robots(Node):
 
     def __init__(self):
         super().__init__("move_robot_node")
-        self.previous_x = 0.0
+
+        # Inital pitch and roll
+        self.pitch = 0.0 # Store pitch
+        self.roll = 0.0 # Store roll
+
+        # Initilise values
         self.last_cmd = None
+        self.initial_scan = None
+        self.robot_distance = None
         self.initialized = False
         self.obstacle_detected = False
-        self.initial_scan = None
         self.aligned = False
-        self.robot_distance = None
-        self.difference_threshold = 0.1  # 10% difference threshold
 
+        # 10% difference threshold for Lidar
+        self.difference_threshold = 0.1  
+
+        # Publishers
         self.cmd_vel_publisher_ = self.create_publisher(Twist, "/agent_0/cmd_vel", 10)
-        self.odometry_subscriber_ = self.create_subscription(Odometry, "/agent_0/odom", self.odometry_callback, 10)
-        self.lidar_subscriber_ = self.create_subscription(LaserScan, "/agent_0/laserscan", self.lidar_callback, 10)
-        self.alignment_subscriber = self.create_subscription(Bool, "/agent_0/alignment", self.alignment_callback, 10)
+
+        # Subscribers
+        self.lidar_subscriber_ = None # Lidar initally inactive
+        self.alignment_subscriber = None # Alignment initally inactive 
+        self.imu_subscriber_ = self.create_subscription(Imu, "/agent_0/imu/out", self.imu_callback, 10) # Always active
+
+        self.get_logger().info("Connecting robots started")
+
+        # Main Script
+        self.timer = self.create_timer(0.1, self.connect_robots)
+
+    def connect_robots(self):
+        if not self.initialized:
+            self.initialized = True
+            linear_x = 0.1
+            self.send_velocity_command(linear_x, 0.0)
+            print("Robot Initialized: Moving forward")
+        else:
+            self.centre_robot()
+            self.lidar_subscriber_ = self.create_subscription(LaserScan, "/agent_0/laserscan", self.lidar_callback, 10)
+            if self.obstacle_detected is True and self.robot_distance < 0.3:
+                # Activate obstacle detection
+                self.get_logger().info("Within 2 cm of obstacle")
+                self.alignment_subscriber = self.create_subscription(Bool, "/agent_0/alignment", self.alignment_callback, 10)
+                if self.aligned is True:
+                    linear_x = 0.01
+                    self.send_velocity_command(linear_x, 0.0)
+                    # SO ROBOT ARE CONNECTED. CHECK WHETHER LOCK CAN LOCK IF DISTANCE IS SMALL ENOUGH.
+                    # ADD LOCIG SO THAT IF THEY ARE NOT ALIGNED, TRIES AGAIN
+
+    def centre_robot(self):
+        k_p = 0.1 # Proportional gain
+
+        if self.roll > 5 or self.roll < -5:
+            angular_z = k_p * self.roll
+            print(f'Angular velocity: {angular_z}')
+        else:
+            angular_z = 0.0
+            self.get_logger().info("Robot centring not needed")
         
-        self.get_logger().info("Robot controller with LiDAR obstacle detection started")
+        self.send_velocity_command(self.last_cmd, angular_z)
 
     def send_velocity_command(self, linear_x, angular_z):
         cmd = Twist()
-        if self.obstacle_detected and self.robot_distance < 0.2 and self.aligned is True:
-            cmd.linear.x = 0.01  # Stop when obstacle is detected
-            print(f"Obstacle detected at {self.robot_distance}! Moving Slower")
-        else:
-            cmd.linear.x = linear_x
-            cmd.angular.z = angular_z
+        cmd.linear.x = linear_x
+        cmd.angular.z = angular_z
 
         if self.last_cmd != cmd.linear.x:
             self.cmd_vel_publisher_.publish(cmd)
             self.last_cmd = cmd.linear.x
-            self.get_logger().info(f'Published command: linear_x = {cmd.linear.x}')
+            print(f'Published command: linear_x = {cmd.linear.x}, angular_z = {cmd.angular.z}')
+    
+    def imu_callback(self, msg: Imu):
+        # Calculate pitch and roll from acclerometer data
+        ax = msg.linear_acceleration.x
+        ay = msg.linear_acceleration.y
+        az = msg.linear_acceleration.z
 
-    def odometry_callback(self, msg: Odometry):
-        position = msg.pose.pose.position
-        x = position.x
-        # self.get_logger().info(f'Position -> x: {x:.2f}')
-
-        if not self.initialized:
-            self.initialized = True
-            self.send_velocity_command(0.1, 0.0)
-            self.get_logger().info("Initialized: Moving forward")
-            return
+        self.roll = math.atan2(ay, az)  # Roll calculation
+        self.pitch = math.atan2(-ax, math.sqrt(ay**2 + az**2))  # Pitch calculation
 
     def lidar_callback(self, msg: LaserScan):
-        if self.initial_scan is None:
+        if not self.initial_scan:
             self.initial_scan = msg.ranges
-            self.send_velocity_command(0.1, 0.0)  # Start moving immediately
             self.get_logger().info("Initial LiDAR scan stored.")
             return
 
@@ -69,10 +108,9 @@ class MyNode(Node):
         if robot_ranges:
             self.robot_distance = np.mean(robot_ranges)
             self.obstacle_detected = True
+            self.get_logger().info(f"Obstacle Detected. Distance to object: {self.robot_distance}")
         else:
             self.obstacle_detected = False
-
-        self.send_velocity_command(self.last_cmd, 0.0)
     
     def alignment_callback(self, msg: Bool):
         if msg.data is True:
@@ -84,7 +122,7 @@ class MyNode(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = MyNode()
+    node = Connect_Robots()
     rclpy.spin(node)
     rclpy.shutdown()
 
