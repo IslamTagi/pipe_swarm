bcac6f1adf814faa4403ddd140bf2783392bd752
Fintan <mn20fl@leeds.ac.uk>
2025-03-18 17:08:13 +0000

    Centre robot update to work with IMU

--- ros_ws/src/pipe_swarm/py_src/centre_robot.py
+++ ros_ws/src/pipe_swarm/py_src/centre_robot.py
@@ -3,6 +3,9 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Range
+from sensor_msgs.msg import Imu
+import math
+from nav_msgs.msg import Odometry
 
 class MyNode(Node):
 
@@ -15,36 +18,48 @@ class MyNode(Node):
         self.cmd_vel_publisher_ = self.create_publisher(Twist, "/agent_0/cmd_vel", 10)
         
         # Create subscriptions for ToF sensors
-        self.left_tof_subscriber_ = self.create_subscription(Range, "/agent_0/left_infrared_range", self.left_tof_callback, 10)
-        self.right_tof_subscriber_ = self.create_subscription(Range, "/agent_0/right_infrared_range", self.right_tof_callback, 10)
+        # self.left_tof_subscriber_ = self.create_subscription(Range, "/agent_0/left_infrared_range", self.left_tof_callback, 10)
+        # self.right_tof_subscriber_ = self.create_subscription(Range, "/agent_0/right_infrared_range", self.right_tof_callback, 10)
+        self.imu_subscriber_ = self.create_subscription(Imu, "/agent_0/imu/out", self.imu_callback, 10)
+        self.odometry_subscriber_ = self.create_subscription(Odometry, "/agent_0/odom", self.odometry_callback, 10) # Used for graph
         
         self.left_distance = None
         self.right_distance = None
         self.get_logger().info("Robot centre controller has started")
 
         # Timer to periodically publish adjust_position (every 0.1 seconds)
-        self.create_timer(1.0, self.adjust_position)
+        self.create_timer(0.1, self.adjust_position)
 
-    def left_tof_callback(self, msg: Range):
-        self.left_distance = msg.range
+    # def left_tof_callback(self, msg: Range):
+    #     self.left_distance = msg.range
 
-    def right_tof_callback(self, msg: Range):
-        self.right_distance = msg.range
+    # def right_tof_callback(self, msg: Range):
+    #     self.right_distance = msg.range
+
+    def odometry_callback(self, msg: Odometry):
+        position = msg.pose.pose.position
+        self.x = position.x
+        self.y = position.y
+        self.z = position.z
+    
+    def imu_callback(self, msg: Imu):
+        # Calculate pitch and roll from acclerometer data
+        ax = msg.linear_acceleration.x
+        ay = msg.linear_acceleration.y
+        az = msg.linear_acceleration.z
+
+        self.pitch = math.degrees(math.atan2(ay, math.sqrt(ax**2 + az**2)))  # Roll calculation
+        self.roll = math.degrees(math.atan2(-ax, math.sqrt(ay**2 + az**2)))  # Pitch calculation
 
     def adjust_position(self):
-        print(f'Left Distance: {self.left_distance}, Right Distance: {self.right_distance}')
-        
-        if self.left_distance is None or self.right_distance is None:
-            return
-        
-        # Compute the error (difference between left and right distances)
-        error = self.left_distance - self.right_distance
-        
-        # Set a proportional gain
-        k_p = 0.1  # Adjust this value based on how aggressively you want to correct
-        angular_z = k_p * error  # Negative to turn towards the closer wall
-        print(f'Angular Velocity: {angular_z}') # +ve value means moving left, -ve means moving right
-        
+        k_p = -0.2 # Proportional gain
+
+        if self.roll > 0.1 or self.roll < -0.1:
+            angular_z = k_p * self.roll
+        else:
+            angular_z = 0.0
+
+        print(f'roll: {self.roll} x: {self.x} y: {self.y} Angular velocity: {angular_z}')
         # Publish velocity command
         # cmd = Twist()
         linear_x = 0.1
@@ -63,7 +78,7 @@ class MyNode(Node):
             self.cmd_vel_publisher_.publish(cmd)
             self.last_linear_x_cmd = linear_x
             self.last_angular_z_cmd = angular_z
-            self.get_logger().info(f'Published command: linear_x = {linear_x}, angular_z = {angular_z}')
+            # print(f'Published command: linear_x: {linear_x}, angular_z: {angular_z}')
 
 
 def main(args=None):
