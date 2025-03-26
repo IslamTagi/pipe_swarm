e32b68536ed631506ba79e109399d8dc44a2e25b
Fintan <mn20fl@leeds.ac.uk>
2025-03-26 09:50:54 +0000

    update to connect robots

--- ros_ws/src/pipe_swarm/py_src/connect_robots.py
+++ ros_ws/src/pipe_swarm/py_src/connect_robots.py
@@ -11,6 +11,7 @@ import math
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from control_msgs.msg import JointTrajectoryControllerState
 from builtin_interfaces.msg import Duration
+from sensor_msgs.msg import Range
 
 class Connect_Robots(Node):
 
@@ -21,10 +22,14 @@ class Connect_Robots(Node):
         self.pitch = 0.0 # Store pitch
         self.roll = 0.0 # Store roll
         self.count = 0.0 # Count to wait before engaging lock
+        self.robot_detection_count = 0.0 # Count to wait before determining robot moved
+        self.align_count = 0 # Count to wait before checking alignment the opposite side
 
         # Initilise values
-        self.last_cmd_agent_0 = None
-        self.last_cmd_agent_1 = None
+        self.last_linear_cmd_agent_0 = None
+        self.last_linear_cmd_agent_1 = None
+        self.last_angular_cmd_agent_0 = None
+        self.last_angular_cmd_agent_1 = None
         self.initial_scan = None
         self.robot_scan = None
         self.robot_distance = None
@@ -34,15 +39,17 @@ class Connect_Robots(Node):
         # True false statements
         self.delay_occured = False
         self.initialized = False
+        self.sonething_detected = False
         self.obstacle_detected = False
         self.aligned = False
         self.robot_detected = False
         self.unlocked = False
         self.locked = False
         self.connected = False
+        self.close = False
 
         # 10% difference threshold for Lidar and IMU
-        self.difference_threshold = 0.1  
+        self.difference_threshold = 0.2  
 
         # Publishers
         self.cmd_vel_publisher_agent_0_ = self.create_publisher(Twist, "/agent_0/cmd_vel", 10)
@@ -52,12 +59,11 @@ class Connect_Robots(Node):
 
         # Subscribers always active
         self.imu_subscriber_ = self.create_subscription(Imu, "/agent_0/imu/out", self.imu_callback, 10)
-        self.lidar_subscriber_ = self.create_subscription(LaserScan, "/agent_0/laserscan", self.lidar_callback, 10)
+        self.front_tof_subscriber_ = self.create_subscription(Range, "/agent_0/infrared_range", self.infrared_range, 10)
         self.alignment_subscriber = self.create_subscription(Bool, "/agent_0/alignment", self.alignment_callback, 10)
         self.male_joint_subscriber = self.create_subscription(JointTrajectoryControllerState, '/agent_0/male_joint_trajectory_controller/controller_state', self.male_joint_state_callback, 10)
-        # self.female_joint_subscriber = self.create_subscription(JointTrajectoryControllerState, '/agent_0/female_joint_trajectory_controller/controller_state', self.female_joint_state_callback, 10)
 
-        print("Connecting robots started")
+        print("Aligning robots started")
 
         # 2 second initial delay
         if self.delay_occured is False:
@@ -75,7 +81,7 @@ class Connect_Robots(Node):
     def connect_robots(self):
         if not self.delay_occured:
             return
-
+        
         # If not moving forward, move forward and initilise IMU, male and female angles
         if not self.initialized:
             self.initialized = True
@@ -113,11 +119,14 @@ class Connect_Robots(Node):
             
             if self.robot_detected is True:
                 # Robot 0 Move forward
-                linear_x = 0.01
-                self.send_velocity_command_agent_0(linear_x, 0.0)
+                # linear_x = 0.05
+                # self.send_velocity_command_agent_0(linear_x, 0.0)
+
+                print(f'robot distance: {self.robot_distance}')
 
                 # Stop if distance is less than 2 cm
-                if self.robot_distance < 0.2:
+                if self.robot_distance < 0.15:
+                    self.close = True
                     print("Within 2 cm of obstacle")
                     # check alignment
                     if self.aligned is True:
@@ -137,7 +146,7 @@ class Connect_Robots(Node):
                             self.lock_male()
 
                             # Check whether lock is in locked position
-                            if self.locked is True:
+                            if self.locked is True: 
                                 print('Locked')
 
                                 # Test whether robots are successfully locked together
@@ -155,32 +164,56 @@ class Connect_Robots(Node):
                                         self.send_velocity_command_agent_1(linear_x, 0.0)
                                         self.send_velocity_command_agent_0(linear_x, 0.0)
                                         self.delay_occured = False
-
+                    
                     else:
                         # ROBOTS NOT ALIGNED FILL IN
+                        # Stop whilst aligning
                         linear_x = 0.0
                         self.send_velocity_command_agent_0(linear_x, 0.0)
+                        self.send_velocity_command_agent_1(linear_x, 0.0)
+
+                        self.align_robots()
+                        return
+                else:
+                    self.close = False
+                    linear_x = 0.02
+                    self.send_velocity_command_agent_0(linear_x, 0.0)
+
+
+    def align_robots(self):
+        print(f'Alignment count: {self.align_count}')
+        self.align_count += 1
+        if self.align_count <= 99:
+            angular_z = 0.2
+            self.send_velocity_command_agent_1(0.0, angular_z)
+        elif self.align_count <= 300:
+            angular_z = -0.2
+            self.send_velocity_command_agent_1(0.0, angular_z)
+        elif self.align_count > 300:
+            self.align_count = 0
+            print('Robot alignment failed')
 
 
     def centre_robot(self):
-        k_p = -0.1 # Proportional gain
+        k_p = -0.05 # Proportional gain
 
-        if self.roll > 5 or self.roll < -5:
+        if self.roll > 0.1 or self.roll < -0.1:
             angular_z = k_p * self.roll
         else:
             angular_z = 0.0
         
         # print(f'roll: {self.roll} Angular velocity: {angular_z}')
-        self.send_velocity_command_agent_0(self.last_cmd_agent_0, angular_z)
+        self.send_velocity_command_agent_0(self.last_linear_cmd_agent_0, angular_z)
 
     def send_velocity_command_agent_0(self, linear_x, angular_z):
         cmd = Twist()
         cmd.linear.x = linear_x
         cmd.angular.z = angular_z
 
-        if self.last_cmd_agent_0 != cmd.linear.x:
+        if self.last_linear_cmd_agent_0 != cmd.linear.x or self.last_angular_cmd_agent_0 != cmd.angular.z:
             self.cmd_vel_publisher_agent_0_.publish(cmd)
-            self.last_cmd_agent_0 = cmd.linear.x
+            self.last_linear_cmd_agent_0 = cmd.linear.x
+            self.last_angular_cmd_agent_0 = cmd.angular.z
             print(f'Published command to agent 0: linear_x = {cmd.linear.x}, angular_z = {cmd.angular.z}')
         
     def send_velocity_command_agent_1(self, linear_x, angular_z):
@@ -188,9 +221,10 @@ class Connect_Robots(Node):
         cmd.linear.x = linear_x
         cmd.angular.z = angular_z
 
-        if self.last_cmd_agent_1 != cmd.linear.x:
+        if self.last_linear_cmd_agent_1 != cmd.linear.x or self.last_angular_cmd_agent_1 != cmd.angular.z:
             self.cmd_vel_publisher_agent_1_.publish(cmd)
-            self.last_cmd_agent_1 = cmd.linear.x
+            self.last_linear_cmd_agent_1 = cmd.linear.x
+            self.last_angular_cmd_agent_1 = cmd.angular.z
             print(f'Published command to agent 1: linear_x = {cmd.linear.x}, angular_z = {cmd.angular.z}')    
 
     
@@ -209,76 +243,81 @@ class Connect_Robots(Node):
         # If robot has already been detected, skip
         if not self.robot_detected:
             # Take an initial scan of LiDAR
-            if not self.initial_scan:
+            if self.initial_scan is None:
                 self.initial_scan = self.range
-                print("Initial LiDAR scan stored.")
+                print(f"Initial LiDAR scan stored: {self.initial_scan}")
                 return
 
             # Calculate the range as the mean difference between the inital scan and the lidar scan. ANGLE NOT USED YET
-            object_range = []
-            for i in range(len(self.range)):
-                if self.range[i] > 0 and self.initial_scan[i] > 0:
-                    difference = abs(self.range[i] - self.initial_scan[i]) / self.initial_scan[i]
-                    if difference > self.difference_threshold:
-                        object_range.append(self.range[i])
-            
-            # If the object range changes, obstacle detected
-            if object_range:
-                # Distance calculation
-                self.robot_distance = np.mean(object_range)
-                self.obstacle_detected = True
-                print(f"Obstacle Detected. Determining whether its a robot")
-                
+
+            difference = abs(self.range - self.initial_scan) / self.initial_scan
+            if difference > self.difference_threshold:
+                print("Obstacle Detected")
+                object_range = self.range
+                self.sonething_detected = True
+
+            if self.sonething_detected is True:
+                print('Determining whether obstacle is a robot')
+
                 # Stop robot when obstacle detected
                 linear_x = 0.0
                 self.send_velocity_command_agent_0(linear_x, 0.0)
 
                 # When object detected. Store scan of object
-                if not self.robot_scan:
+                if self.robot_scan is None:
                     self.robot_scan = self.range
-                    print(f'Robot scan stored')
+                    print(f'Robot scan stored: {self.robot_scan}')
                     return
 
                 # Move robot 1 forward
                 linear_x = 0.1
                 self.send_velocity_command_agent_1(linear_x, 0.0)
 
-                robot_range = []
-                for i in range(len(self.range)):
-                    if self.range[i] > 0 and self.robot_scan[i] > 0:
-                        robot_difference = abs(self.range[i] - self.robot_scan[i]) / self.robot_scan[i]
-                        if robot_difference > self.difference_threshold:
-                            robot_range.append(self.range[i])
-                
-                if robot_range:
+                robot_difference = abs(self.range - self.robot_scan) / self.robot_scan
+                if robot_difference > self.difference_threshold:
                     print('Robot detected')
                     self.robot_detected = True
+                
+                if self.robot_detected is True:
+                    self.robot_distance = self.range
+
+                    # Stop robot 1
                     linear_x = 0.0
                     self.send_velocity_command_agent_1(linear_x, 0.0)
+
+                    # Start robot 0
+                    linear_x = 0.1
+                    self.send_velocity_command_agent_0(linear_x, 0.0)
                     # EXIT ONE: ROBOT DETECTED
-                else:
+                elif self.robot_detection_count > 5:
                     print('Robot not detected - obstacle detected')
+                    self.obstacle_detected = True
                     # Thfis is triggering immediately. Create a count to wait to 2 loops before concluding this
                     linear_x = 0.0
                     self.send_velocity_command_agent_1(linear_x, 0.0)
                     # EXIT TWO: OBJECT DETECTED
 
+                self.robot_detection_count = self.robot_detection_count + 1
+            
             else:
                 self.obstacle_detected = False
                 print('No obstacle detected')
                 # EXIT THREE: NO OBJECT DETECTED
                 return
         else:
+            self.robot_distance = self.range
             return
 
-    def lidar_callback(self, msg: LaserScan):
+    def infrared_range(self, msg: Range):
         # Two exits to robot detection, either yes robot detected or only object detected
-        self.range = msg.ranges
+        self.range = msg.range
 
     
     def alignment_callback(self, msg: Bool):
         if msg.data is True:
             self.aligned = True
+        if msg.data is False:
+            self.aligned = False
 
     def male_joint_state_callback(self, msg):
         # Input position
