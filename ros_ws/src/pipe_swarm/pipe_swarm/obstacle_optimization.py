b296ce165cd1e2052800794b5be5eb37f559a113
islamtagi <islamtagi@hotmail.com>
2025-04-11 21:07:27 +0100

    adding function to get robot plan state

--- ros_ws/src/pipe_swarm/pipe_swarm/obstacle_optimization.py
+++ ros_ws/src/pipe_swarm/pipe_swarm/obstacle_optimization.py
@@ -283,12 +283,12 @@ class Obstacle():
 
 class ModularConfiguration():
 
-    def __init__(self, sigma_np, x_pos, l_agent, thickness_agent, m_agent,
+    def __init__(self, sigma_np, x_pos, l_agent, thickness_agent, m_agent, x_pos_init,
                 agent_pivot_offset=1.75):
 
         # global reference frame
         y_offset = thickness_agent/2 + agent_pivot_offset
-        self.global_coordinates = (0, y_offset+1e-5)
+        self.global_coordinates = (x_pos_init, y_offset+1e-5)
         self.theta_global = 0
 
         # modular robot parameters
@@ -496,8 +496,8 @@ class ModularConfiguration():
 
 class ModelPredictiveControl():
 
-    def __init__(self, n_agents, l_agent, thickness_agent, m_agent, obstacle:Obstacle,
-                 x_pos_min=-10, x_pos_max=10, sigma_min=-45, sigma_max=45):
+    def __init__(self, n_agents, l_agent, thickness_agent, m_agent, x_pos_init, obstacle:Obstacle,
+                 x_pos_min=-25, x_pos_max=25, sigma_min=-45, sigma_max=45):
 
         # defining model
         self.n_agents = n_agents
@@ -515,7 +515,7 @@ class ModelPredictiveControl():
         self.theta0_bounds = [(sigma_min, sigma_max) for _ in range(n_agents)] # sigma bounds
         self.theta0_bounds.insert(0, (x_pos_min, x_pos_max)) # x pos bounds
         
-        self.model_config = ModularConfiguration(self.sigma, self.x_pos, l_agent, thickness_agent, m_agent)
+        self.model_config = ModularConfiguration(self.sigma, self.x_pos, l_agent, thickness_agent, m_agent, x_pos_init)
 
           # objective function
         self.pos_desired = (0, 0)
--- ros_ws/src/pipe_swarm/py_src/goal_state_optimizer.py
+++ ros_ws/src/pipe_swarm/py_src/goal_state_optimizer.py
@@ -2,7 +2,7 @@
 import rclpy
 from pipe_swarm.obstacle_optimization import Obstacle, Pipe, ModelPredictiveControl
 from rclpy.node import Node
-from std_msgs.msg import Float64MultiArray 
+from std_msgs.msg import Float64MultiArray, Bool
 
 class GoalStateNode(Node):
     def __init__(self):
@@ -10,6 +10,9 @@ class GoalStateNode(Node):
         self.get_logger().info("Goal State Solver Active")
         self.goal_state_publisher = self.create_publisher(Float64MultiArray , "/modular_goal_state", 20)
         self.pipe_pos_publisher = self.create_publisher(Float64MultiArray , "/define_pipe_positions", 20)
+        self.goal_plan_subscriber = self.create_subscription(Bool, "/plan_successful", self.get_plan_accepted, 20)
+        self.got_plan_response = False
+        self.plan_accepted = False
 
     def send_goal_state(self, message):
         goal_state_msg = Float64MultiArray ()
@@ -22,60 +25,121 @@ class GoalStateNode(Node):
         pipe_pos_msg.data = [float(x) for x in message]
         self.pipe_pos_publisher.publish(pipe_pos_msg)
         self.get_logger().info(f"Sent Pipe Pos: {pipe_pos_msg.data}")
+    
+    def get_plan_accepted(self, message : Bool):
+        self.got_plan_response = True
+        self.plan_accepted = message.data
+        print(self.plan_accepted)
+        self.get_logger().info(f"Goal Plan Accepted: {self.plan_accepted}")
 
 QUE_SIZE = 10
 
+def get_robot_plan(x_starting_pos, obstacle, goal_endpoint, 
+                   pipe_1_origin, pipe_2_origin):
+    
+    success = False
+    num_agents_init = 3
+    max_retries_of_n_agents = 15
+    max_agents = 5
+
+    theta_solution = None
+
+    for num_agents in range(num_agents_init, max_agents+1):
+        for i in range(1, max_retries_of_n_agents +1):
+            
+            print(f'\nIteration: {i} of {num_agents} agents')
+            new_starting_pos = x_starting_pos-(num_agents-1)*l_agent # for 3 agents, start 2 agents behind origin
+            print(f'New starting pos: {new_starting_pos}')
+            
+            mpc = ModelPredictiveControl(num_agents, l_agent, thickness_agent, m_agent, new_starting_pos, obstacle)
+            theta_solution = mpc.inverse_kinematics_with_constraints(goal_endpoint)
+            
+            if theta_solution is not None:
+                # if found a potential solution that is closer than 1cm
+                if mpc.objective(theta_solution) > 1:
+                    continue
+                print(f"SUCCESS FOUND WITH: {theta_solution}")
+
+                # send solution to ROS and await confirmation of planned state
+                ros_solution = mpc.model_config.reformat_solution(theta_solution)
+                node.send_goal_state(ros_solution)
+                while not node.got_plan_response:
+                    rclpy.spin_once(node, timeout_sec=0.001) # refresh for callback
+                node.got_plan_response = False # clear for potential fail
+                if(True == node.plan_accepted):
+                    node.plan_accepted = False # reset
+                    success = True
+                    break
+        
+        if (True == success):
+            break
+        else:
+            theta_solution = None # clear from any successes
+
+    mpc.model_config.visualize_agent_configuration(obstacle)
+
+    return num_agents, theta_solution
+        
+
+
+# rclpy.init()
+node = GoalStateNode()
 
 # define agent
 l_agent = 15 #cm
 thickness_agent = 6.2 + 1.5 # base_h + wheel_r
 m_agent = 0.175 # kg
-n_agents = 3    # number of agents
-
-# define pipe
-pipe_1_x = 0 # tail
-pipe_1_origin = [pipe_1_x, 0]
-pipe_radius = 7.5
-pipe_thickness = 5
-pipe_length = n_agents*l_agent
-
-pipe_2_x = pipe_length + pipe_1_x
-pipe_2_origin = [pipe_length + pipe_1_x, 2.5]
-
-pipe = Pipe(pipe_length, pipe_radius, pipe_thickness, pipe_1_origin)
-obstacle = pipe.get_obstacle()
-
-step_pipe = Pipe(pipe_length, pipe_radius, pipe_thickness, pipe_2_origin)
-step_pipe_obstacle = step_pipe.get_obstacle()
-
-obstacle_x, obstacle_y = obstacle.get_overall_obstacle(step_pipe_obstacle)
-obstacle = Obstacle(obstacle_x, obstacle_y)
 
 
 def main(args=None):
 
-    rclpy.init()
-    node = GoalStateNode()
+    # define agent
+
+    # define pipe
+    pipe_radius = 7.5
+    pipe_thickness = 5
+    pipe_length = 75
+    
+    pipe_1_origin = [-25, 0]
+    pipe_2_origin = [70, 5]
+
+    pipe = Pipe(pipe_length, pipe_radius, pipe_thickness, pipe_1_origin)
+    obstacle = pipe.get_obstacle()
+
+    step_pipe = Pipe(pipe_length, pipe_radius, pipe_thickness, pipe_2_origin)
+    step_pipe_obstacle = step_pipe.get_obstacle()
+
+    obstacle_x, obstacle_y = obstacle.get_overall_obstacle(step_pipe_obstacle)
+    obstacle = Obstacle(obstacle_x, obstacle_y)
 
     pipe_pos = [pipe_length, pipe_radius, pipe_thickness]
     pipe_pos.extend(pipe_1_origin) # x,y
     pipe_pos.extend(pipe_2_origin) # x,y
     node.send_pipe_pos(pipe_pos)
-    
-    mpc = ModelPredictiveControl(n_agents, l_agent, thickness_agent, m_agent, obstacle)
-    theta_solution = mpc.inverse_kinematics_with_constraints((pipe_2_x+3, 3))
 
-    node.get_logger().info(f'Goal Endpoints: {mpc.model_config.endpoints}')
-    if theta_solution is not None:
-        ros_solution = mpc.model_config.reformat_solution(theta_solution)
-        node.send_goal_state(ros_solution)
-        mpc.model_config.visualize_agent_configuration(obstacle)
+    x_pos = 35
+
+    n_agents, solution = get_robot_plan(x_pos, obstacle, (70, 5), pipe_1_origin, pipe_2_origin)
+    if solution is not None:
+        print(f"{n_agents} Agents Needed")
     else:
-        node.get_logger().warn("Failed to find goal state")
-        mpc.model_config.visualize_agent_configuration(obstacle)
-    # mpc.model_config.visualize_agent_configuration(obstacle)
+        print("NO SOLUTION FOUND")
+
     
-    rclpy.shutdown()
+    # mpc = ModelPredictiveControl(n_agents, l_agent, thickness_agent, m_agent, obstacle)
+    # theta_solution = mpc.inverse_kinematics_with_constraints((pipe_2_x+3, 3))
+
+    # node.get_logger().info(f'Goal Endpoints: {mpc.model_config.endpoints}')
+    # if theta_solution is not None:
+    #     ros_solution = mpc.model_config.reformat_solution(theta_solution)
+    #     node.send_goal_state(ros_solution)
+    #     mpc.model_config.visualize_agent_configuration(obstacle)
+    # else:
+    #     node.get_logger().warn("Failed to find goal state")
+    #     mpc.model_config.visualize_agent_configuration(obstacle)
+    # # mpc.model_config.visualize_agent_configuration(obstacle)
+    
+    # rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
